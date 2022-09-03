e6ccdf63c44247418895877ad3aa0e800d80f372
Kyril <moreunderground@gmail.com>
2022-09-03 21:24:53 +0300

    first commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDao.java
@@ -0,0 +1,14 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+import ru.kata.spring.boot_security.demo.models.User;
+import java.util.List;
+
+public interface UserDao {
+    void saveUser(User user);
+    User getUserById(int id);
+    void updateUser(User user);
+    void deleteUserById(int id);
+    List<User> getAllUsers();
+    User findUserByUsername(String name);
+}
+
